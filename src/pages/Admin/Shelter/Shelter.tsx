0f66336959cbbae5cdadf07a66fb45a8600250c1
LorenzoLopes <lorenzolopes.dev@gmail.com>
2024-04-23 22:15:59 -0300

    feat: Input component

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,9 +1,14 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
-
+import styles from "./Shelter.module.css"
 export function Shelter(){
     return(
         <Panel> 
-            shelter
+            <form className={styles.container} onSubmit={submit} > 
+            <Input label="Nome" />
+            <Button type="submit">salvar</Button>
+            </form>
         </Panel>
     )
 }
