32f6486d0834c12dce9d00d90341ab7a5271ea99
Lorenzo-Lopes <lorenzolopes.dev@gmail.com>
2024-04-26 11:48:57 -0300

    alteração de css

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useHookFormMask } from "use-mask-input";
 import { toast } from "sonner";
 import { updateShelter } from "../../../services/shelter/updateShelter";
-import {  useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 const shelterSchema = z.object({
   name: z
@@ -16,46 +16,42 @@ const shelterSchema = z.object({
     .min(2, "nome deve ter no minimo 2 caracteres")
     .max(30, "Nomde deve ter no maximo 30."),
   email: z.string().email("Campo deve ser um email"),
-  phone: z.string().refine(
-    (value) => {
+  phone: z.string().refine((value) => {
       const digits = value.replace(/\D/g, "").length;
       return digits >= 10 && digits <= 11;
-    },
-    { message: "Numero deve ter entre 10 e 11 numeros" }
-  ),
+    }, "Numero deve ter entre 10 e 11 numeros"),
   whatsApp: z.string().refine(
     (value) => {
       const digits = value.replace(/\D/g, "").length;
       return digits >= 10 && digits <= 11;
-    },
-    { message: "Numero deve ter entre 10 e 11 numeros" }
-  ),
+    }, "Numero deve ter entre 10 e 11 numeros" ),
 });
 
 type ShelterSchema = z.infer<typeof shelterSchema>;
 
+// export function Shelter() {
+//   const { handleSubmit, register, formState } = useForm<ShelterSchema>({
+//     resolver: zodResolver(shelterSchema),
+//   });
 export function Shelter() {
   const { handleSubmit, register, formState } = useForm<ShelterSchema>({
     resolver: zodResolver(shelterSchema),
   });
 
   const registerwithMask = useHookFormMask(register);
-  const queryClient = useQueryClient()
-  async function submit({ name, email, phone, whatsApp }: ShelterSchema) {
-    console.log(name, email, phone, whatsApp);
+  const queryClient = useQueryClient();
 
+  async function submit({ name, email, phone, whatsApp }: ShelterSchema) {
     const toastid = toast.loading("Salvando Dados");
     try {
-      
       await updateShelter({
         name,
         email,
         phone: phone.replace(/\D/g, ""),
         whatsApp: whatsApp.replace(/\D/g, ""),
-      })
-      queryClient.invalidateQueries({queryKey:['get-shelter']})
-      toast.success("salvou caraio"),
-        {
+      });
+      queryClient.invalidateQueries({ queryKey: ["get-shelter"] });
+      toast.success("salvou caraio"), {
           id: toastid,
           closeButton: true,
         };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,52 @@
 import { Button, ButtonVariant } from "../../components/common/Button";
 
-import styles from './Home.module.css'
-import dog from './../../assets/dog.svg'
+import styles from "./Home.module.css";
+import dog from "./../../assets/dog.svg";
 import { Link } from "react-router-dom";
-export function Home(){
-    return (
-      <div className={styles.container}>
-        <img src={dog} width="278.2px"/>
-
-        <Link to="/pets">
-          <Button variant={ButtonVariant.Default}>Quero Adotar</Button>
-        </Link>
-        <Link to="/admin">
-          <Button variant={ButtonVariant.Outlined}>Tenho um Abrigo</Button>
-        </Link>
+export function Home() {
+  return (
+    <div className={styles.home}>
+      <div className={styles.loginContainer}>
+        <div className={styles.loginContent}>
+          <div className={styles["loginContentHeader"]} id="logo">
+            <h2>
+              Bem-vindos ao <strong>Luz dos bichos</strong>
+            </h2>
+            <p>
+              Aqui, cada animal é uma estrela à espera de um novo começo.
+              Junte-se a nós em nossa missão de oferecer amor, cuidado e um lar
+              seguro para nossos amigos peludos. Explore como você pode ajudar
+              ou adotar e mudar uma vida hoje!
+            </p>
+          </div>
+          <div>
+            <form className={styles["login-form"]}>
+              <Link to="/pets" className={styles.link}>
+                <Button
+                  variant={ButtonVariant.Default}
+                  
+                 
+                >
+                 Quero Adotar
+                </Button>
+              </Link>
+              <Link to="/admin" className={styles.link}>
+                <Button
+                  variant={ButtonVariant.Outlined}
+                >
+                  Tenho um abrigo
+                </Button>
+              </Link>
+            </form>
+            <p>
+              Projeto de Desenvolvimento curso FullStack Pecege.
+              <br />
+              <strong>Termos e Serviços</strong> e{" "}
+              <strong>Politica de Privacidade</strong>.
+            </p>
+          </div>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
